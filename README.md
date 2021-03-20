0422ee9aeae24b1da72a63f764ea8b67146f7710
Ian Fisher <iafisher@fastmail.com>
2021-03-19 18:02:50 -0700

    Improve readme and Vue page

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-Minimal example of a Vue app with single-file components using NPM and Webpack, without all the bells and whistles that the Vue CLI adds. Produces a bundled JavaScript file suitable for use in an existing web application.
+A minimal example of a Vue app. Produces a bundled JavaScript file suitable for use in an existing web application.
+
+Clone it to jump-start your own Vue project:
 
 ```
 git clone https://github.com/iafisher/minimal-vue-app.git
@@ -7,4 +9,10 @@ npm install
 npm run build
 ```
 
-Then open `index.html` in a browser.
+Then, add
+
+```
+<script src="dist/main.js"></script>
+```
+
+to your HTML template, and start editing `components/App.vue`.
--- components/App.vue
+++ components/App.vue
@@ -1,19 +1,104 @@
 <template>
-  <p>Hello, {{ name }}!</p>
-</template>
+  <main>
+    <h1>Minimal Vue App</h1>
+    <p>
+      This is a minimal example of a Vue app. It builds with
+      <a href="https://webpack.js.org/">Webpack</a> into a single bundled
+      JavaScript file that can be used by any backend framework.
+    </p>
 
+    <p>Clone it to jump-start your own Vue project:</p>
+
+    <pre><code><span class="prompt">&gt;  </span>git clone https://github.com/iafisher/minimal-vue-app.git
+<span class="prompt">&gt;  </span>cd minimal-vue-app
+<span class="prompt">&gt;  </span>npm install
+<span class="prompt">&gt;  </span>npm run build</code></pre>
+
+    <p>Then, add</p>
+    <pre><code>&lt;script src="dist/main.js"&gt;&lt;script&gt;</code></pre>
+    <p>
+      to your HTML template, and start editing <code>components/App.vue</code>.
+      Or, open <code>index.html</code> in your browser to see this page.
+    </p>
+
+    <p>
+      The source code is available at
+      <a href="https://github.com/iafisher/minimal-vue-app"
+        >github.com/iafisher/minimal-vue-app</a
+      >.
+    </p>
+
+    <hr />
+
+    <p>To prove this isn't just a static HTML page...</p>
+    <p>
+      <label
+        >Enter your name and Vue will say hello:
+        <input v-model="name" autocomplete="off" spellcheck="false"
+      /></label>
+    </p>
+    <p v-show="name.length > 0" class="greeting">
+      Hello, <span class="festive">{{ name }}</span
+      >!
+    </p>
+  </main>
+</template>
 
 <script>
 module.exports = {
   data() {
-    return { name: 'world' };
+    return { name: "" };
   },
 };
 </script>
 
-
 <style scoped>
-p {
+* {
+  font-family: sans-serif;
+}
+
+main {
+  max-width: 42rem;
+  margin: 0 auto;
+}
+
+h1 {
   text-align: center;
+}
+
+p {
+  font-size: 1.2rem;
+  line-height: 1.5;
+}
+
+pre {
+  text-align: left;
+  background-color: whitesmoke;
+  padding: 5px 10px;
+  line-height: 1.5;
+}
+
+code {
+  font-family: monospace;
+}
+
+.prompt {
+  user-select: none;
+  opacity: 25%;
+}
+
+input {
+  font-size: 1.2rem;
+}
+
+.greeting {
+  margin-top: 20px;
+  font-size: 1.4rem;
+  text-align: center;
+}
+
+.festive {
+  color: #ff5722;
+  font-weight: bold;
 }
 </style>
